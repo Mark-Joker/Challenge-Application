e4a9f0bf69d59c2c68b9a084d0043fd54042ba4d
Y.Z <y.m.2848@gmail.com>
2024-02-14 08:42:40 +0300

    Add logic for withdraw money.

--- Application/Features/WithdrawMoney.cs
+++ Application/Features/WithdrawMoney.cs
@@ -6,18 +6,51 @@ namespace Application.Features
 {
     public class WithdrawMoney
     {
-        private IAccountRepository accountRepository;
-        private INotificationService notificationService;
+        private readonly IAccountRepository _accountRepository;
+        private readonly INotificationService _notificationService;
 
         public WithdrawMoney(IAccountRepository accountRepository, INotificationService notificationService)
         {
-            this.accountRepository = accountRepository;
-            this.notificationService = notificationService;
+            _accountRepository = accountRepository;
+            _notificationService = notificationService;
         }
 
         public void Execute(Guid fromAccountId, decimal amount)
         {
-            // TODO:
+            if (amount == 0m)
+            {
+                return;
+            }
+
+            if (amount < 0m)
+            {
+                throw new InvalidOperationException("Negative amount can't be processed");
+            }
+
+            var accountToWithdraw = _accountRepository.GetAccountById(fromAccountId);
+
+            if (accountToWithdraw == null)
+            {
+                throw new NullReferenceException("User account was not found");
+            }
+
+            // Check if money is enough to withdraw.
+            var balanceAfterWithdraw = accountToWithdraw.Balance - amount;
+            if (balanceAfterWithdraw < 0m)
+            {
+                throw new InvalidOperationException("Insufficient funds to make withdraw");
+            }
+
+            // Withdraw money.   
+            accountToWithdraw.Balance = accountToWithdraw.Balance - amount;
+            accountToWithdraw.Withdrawn = accountToWithdraw.Withdrawn - amount;
+
+            _accountRepository.Update(accountToWithdraw);
+
+            if (accountToWithdraw.Balance < 500m)
+            {
+                _notificationService.NotifyFundsLow(accountToWithdraw.User.Email);
+            }
         }
     }
 }
